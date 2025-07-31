3882f5a7376e7e64385c44b75956823f46764509
ShriyanshPandey768 <suyogpandeyla@gmail.com>
2025-08-01 00:21:36 +0530

    final2

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const userRoutes = require('./routes/users'); // ← NEW
 console.log('✅ index.js is running');
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+   origin: ['http://localhost:3000', 'https://instapicme.netlify.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
